0db1d1e01b4688463d92ec5b8371392036276516
zfima <zzfima@gmail.com>
2021-08-30 13:22:28 +0300

    impl

--- Python Design Patterns/Creational/Singleton/main.py
+++ Python Design Patterns/Creational/Singleton/main.py
@@ -2,7 +2,8 @@ from singleton import Singleton
 
 if __name__ == '__main__':
     s1 = Singleton(f1="Lambda1")
+    print(s1)
     s2 = Singleton(f2="Lambda2")
+    print(s1)
     s3 = Singleton(f3="Lambda3")
-
-    print(Singleton())
+    print(s1)
