b349c58c6fad042369a3a8edd8d8ce405cd4897a
zfima <zzfima@gmail.com>
2021-08-30 12:18:04 +0300

    rfct

--- Python Design Patterns/Creational/Factory/main.py
+++ Python Design Patterns/Creational/Factory/main.py
@@ -1,9 +1,8 @@
-# Press the green button in the gutter to run the script.
 from factory import get_pet
 
 if __name__ == '__main__':
-    p = get_pet("dog")
-    assert p.speak() == "Woof!"
+    d = get_pet("dog")
+    assert d.speak() == "Woof!"
 
     c = get_pet("cat")
     assert c.speak() == "Meow!"
