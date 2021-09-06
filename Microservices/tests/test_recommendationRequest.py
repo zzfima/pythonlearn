20dff4cacd5dd01c25336efd36f8f966b7e82f98
zzfima <zzfima@gmail.com>
2021-09-06 13:50:41 +0300

    add tests

--- Microservices/tests/test_recommendationRequest.py
+++ Microservices/tests/test_recommendationRequest.py
@@ -5,10 +5,15 @@ from src.recommendations.recommendations_pb2 import BookCategory, Recommendation
 
 def test_recommendation_request_1():
     request = RecommendationRequest(user_id=1, category=BookCategory.SCIENCE_FICTION, max_results=3)
-    assert request.category == 1, "Should be 61"
+    assert request.category == 1, "Should be 1"
 
 
 def test_recommendation_request_2():
     with pytest.raises(TypeError) as excinfo:
         request = RecommendationRequest(user_id="oops", category=BookCategory.SCIENCE_FICTION, max_results=3)
     assert "'oops' has type str, but expected one of: int, long" in str(excinfo.value)
+
+
+def test_recommendation_request_3():
+    request = RecommendationRequest(user_id=1, category=BookCategory.SCIENCE_FICTION)
+    assert request.max_results == 0, "Should be 0"
