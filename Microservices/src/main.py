79f24637eb7b7c884ed63f5359aec8fd206ae7c2
zzfima <zzfima@gmail.com>
2021-09-06 14:13:42 +0300

    rfct

--- Microservices/src/main.py
+++ Microservices/src/main.py
@@ -1,4 +1,2 @@
-from recommendations.recommendations_pb2 import BookCategory, RecommendationRequest
-
 if __name__ == '__main__':
     print("Hello")
--- Microservices/tests/test_recommendationRequest.py
+++ Microservices/tests/test_recommendationRequest.py
@@ -1,6 +1,8 @@
 import pytest
+import grpc
 
 from src.recommendations.recommendations_pb2 import BookCategory, RecommendationRequest
+from src.recommendations.recommendations_pb2_grpc import RecommendationsStub
 
 
 def test_recommendation_request_1():
@@ -17,3 +19,10 @@ def test_recommendation_request_2():
 def test_recommendation_request_3():
     request = RecommendationRequest(user_id=1, category=BookCategory.SCIENCE_FICTION)
     assert request.max_results == 0, "Should be 0"
+
+
+def test_recommendation_request_4():
+    channel = grpc.insecure_channel("localhost:50051")
+    client = RecommendationsStub(channel)
+    request = RecommendationRequest(user_id=1, category=BookCategory.SCIENCE_FICTION, max_results=3)
+    client.Recommend(request)
