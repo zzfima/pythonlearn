0cc70ff7eded5e79025538cc382bea26dae67d54
zfima <zzfima@gmail.com>
2021-09-14 13:18:02 +0300

    rfct

--- Programming Foundations Real World Examples/console1/main.py
+++ Programming Foundations Real World Examples/console1/main.py
@@ -1,6 +1,18 @@
+name = 'koko'
+
+
 def print_hi(*attrs):
     print(len(attrs))
+    print(name)
+
+
+def print_bye():
+    global name
+    name = 'lolo'
+    print(name)
 
 
 if __name__ == '__main__':
     print_hi('dsd', 'dsd', 'fff')
+    print_bye()
+    print_hi()
