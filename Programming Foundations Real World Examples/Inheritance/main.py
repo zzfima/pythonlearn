bf72c7a700bfeeeac06b904032744ceccd224279
zzfima <zzfima@gmail.com>
2021-09-15 12:23:07 +0300

    add queue

--- Programming Foundations Real World Examples/Inheritance/main.py
+++ Programming Foundations Real World Examples/Inheritance/main.py
@@ -1,3 +1,4 @@
+import queue
 from pprint import pprint
 
 if __name__ == '__main__':
@@ -11,3 +12,17 @@ if __name__ == '__main__':
     pprint(list(set(dir(my_list)) - set(dir(my_tuple))))
     print('\n Dif my_tuple - my_list')
     pprint(list(set(dir(my_tuple)) - set(dir(my_list))))
+
+    q = queue.Queue()
+    q.put('a')
+    q.put('ab')
+    q.put('ac')
+    print(q.get())
+    print(q.get())
+
+    try:
+        print(q.get(block=False))
+        print(q.get(block=False))
+    except queue.Empty:
+        print("An exception occurred because of empty queue try get object")
+    print(f'Queue is empty: {q.empty()}')
