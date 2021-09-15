151b98b9f97dfb4f7bd0b58bd2644d3175692987
zzfima <zzfima@gmail.com>
2021-09-15 12:29:49 +0300

    queue full/empty exceptions

--- Programming Foundations Real World Examples/Inheritance/main.py
+++ Programming Foundations Real World Examples/Inheritance/main.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     print('\n Dif my_tuple - my_list')
     pprint(list(set(dir(my_tuple)) - set(dir(my_list))))
 
-    q = queue.Queue()
+    q = queue.Queue(3)
     q.put('a')
     q.put('ab')
     q.put('ac')
@@ -26,3 +26,12 @@ if __name__ == '__main__':
     except queue.Empty:
         print("An exception occurred because of empty queue try get object")
     print(f'Queue is empty: {q.empty()}')
+
+    try:
+        q.put('a', block=False)
+        q.put('ab', block=False)
+        q.put('ac', block=False)
+        q.put('ad', block=False)
+        q.put('ae', block=False)
+    except queue.Full:
+        print("An exception occurred because of full queue try add object")
