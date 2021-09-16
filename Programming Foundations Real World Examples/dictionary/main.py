a8918b93d6296aab93d7ea026edfb27aac22dd68
zzfima <zzfima@gmail.com>
2021-09-16 10:56:51 +0300

    add hash functions

--- Programming Foundations Real World Examples/dictionary/main.py
+++ Programming Foundations Real World Examples/dictionary/main.py
@@ -1,3 +1,15 @@
+import hashlib
+
 if __name__ == '__main__':
-    print(hash('123'))
-    print(hash('123'))
+    # Hash functions
+    print(hash('Hello world'))
+    print(hash('Hello world'))
+    print(type(hash('Hello world')))
+
+    md5_1 = hashlib.md5()
+    md5_1.update(b'Hello world')
+    print(md5_1.hexdigest())
+
+    md5_2 = hashlib.md5()
+    md5_2.update(b'Hello world')
+    print(md5_2.hexdigest())
