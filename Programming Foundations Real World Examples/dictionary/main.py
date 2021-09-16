3e071420b3eec916c6545a6175345da2979565ac
zzfima <zzfima@gmail.com>
2021-09-16 11:28:03 +0300

    calculate md5 file

--- Programming Foundations Real World Examples/dictionary/main.py
+++ Programming Foundations Real World Examples/dictionary/main.py
@@ -6,10 +6,16 @@ if __name__ == '__main__':
     print(hash('Hello world'))
     print(type(hash('Hello world')))
 
-    md5_1 = hashlib.md5()
-    md5_1.update(b'Hello world')
-    print(md5_1.hexdigest())
+    sha256_1 = hashlib.sha256()
+    sha256_1.update(b'Hello world')
+    print(sha256_1.hexdigest())
 
-    md5_2 = hashlib.md5()
-    md5_2.update(b'Hello world')
-    print(md5_2.hexdigest())
+    sha256_2 = hashlib.sha256()
+    sha256_2.update(b'Hello world')
+    print(sha256_2.hexdigest())
+
+    with open(r"C:\Users\zzfim\Downloads\python-3.9.7-amd64.exe", "rb") as file:
+        md5 = hashlib.md5()
+        for chunk in iter(lambda: file.read(1024), b""):
+            md5.update(chunk)
+        print(md5.hexdigest())
