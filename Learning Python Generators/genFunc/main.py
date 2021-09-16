45ea15265991d94c57efd56b9383241373525e48
zzfima <zzfima@gmail.com>
2021-09-16 17:06:29 +0300

    rfct

--- Learning Python Generators/genFunc/main.py
+++ Learning Python Generators/genFunc/main.py
@@ -6,5 +6,13 @@ def even_integers_function(n):
     return result
 
 
+def even_integers_generators(n):
+    result = []
+    for i in range(n):
+        if i % 2 == 0:
+            yield i
+
+
 if __name__ == '__main__':
     print(even_integers_function(11))
+    print(list(even_integers_generators(11)))
