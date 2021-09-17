911b78197b256157f6921a8f762176459e22686d
zzfima <zzfima@gmail.com>
2021-09-17 11:36:20 +0300

    pipeline

---
+++ Learning Python Generators/genFunc/simple_context_manager.py
@@ -0,0 +1,15 @@
+from contextlib import contextmanager
+
+
+@contextmanager
+def simple_cm(n):
+    try:
+        print(f'setup {n}')
+        yield
+    finally:
+        print(f'wrap up {n}')
+
+
+if __name__ == '__main__':
+    with simple_cm(10) as n:
+        pass
