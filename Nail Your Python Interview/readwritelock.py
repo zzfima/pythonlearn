725c3e2d6417d9e0a5ef7cea672cbcce8555a98c
zfima <zzfima@gmail.com>
2021-10-04 15:43:20 +0300

    rfct

--- Nail Your Python Interview/readwritelock.py
+++ Nail Your Python Interview/readwritelock.py
@@ -1,26 +1,37 @@
 import threading
-import time
 
-lock = threading.Lock()
+from readerwriterlock import rwlock
 
-
-def f1():
-    print('f1 starts')
-    lock.acquire()
-    f2()
-    lock.release()
-    print('f1 finished')
+WEEKDAYS = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+today = 0
+marker = rwlock.RWLockFair()
 
 
-def f2():
-    lock.acquire()
-    print('f2 starts')
-    time.sleep(1)
-    print('f2 finished')
-    lock.release()
+def calendar_reader(id_number):
+    global today
+    read_marker = marker.gen_rlock()
+    name = 'Reader-' + str(id_number)
+    while today < len(WEEKDAYS) - 1:
+        read_marker.acquire()
+        print(name, 'sees that today is', WEEKDAYS[today], '-read count:', read_marker.c_rw_lock.v_read_count)
+        read_marker.release()
+
+
+def calendar_writer(id_number):
+    global today
+    write_marker = marker.gen_wlock()
+    name = 'Writer-' + str(id_number)
+    while today < len(WEEKDAYS) - 1:
+        write_marker.acquire()
+        today = (today + 1) % 7
+        print(name, 'updated date to ', WEEKDAYS[today])
+        write_marker.release()
 
 
 if __name__ == '__main__':
-    th1 = threading.Thread(target=f1, name='th1')
-    th1.start()
-    th1.join()
+    # create ten reader threads
+    for i in range(10):
+        threading.Thread(target=calendar_reader, args=(i,)).start()
+    # ...but only two writer threads
+    for i in range(2):
+        threading.Thread(target=calendar_writer, args=(i,)).start()
