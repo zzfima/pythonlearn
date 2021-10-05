c01148fe5ceb925c294dc505c0b25fc9e71b473d
zzfima <zzfima@gmail.com>
2021-10-05 20:46:21 +0300

    rfct

--- Nail Your Python Interview/deadlock.py
+++ Nail Your Python Interview/deadlock.py
@@ -10,18 +10,21 @@ sushi_amount = 500
 def ef(first_chopstick, second_chopstick):
     global sushi_amount
     while sushi_amount > 0:
-        first_chopstick.acquire()
-        second_chopstick.acquire()
+        try:
+            first_chopstick.acquire()
+            second_chopstick.acquire()
 
-        if sushi_amount > 0:
-            sushi_amount -= 1
-            print(threading.currentThread().getName() + ', sushi remained: ' + str(sushi_amount))
-
-        second_chopstick.release()
-        first_chopstick.release()
+            if sushi_amount > 0:
+                sushi_amount -= 1
+                print(threading.currentThread().getName() + ', sushi remained: ' + str(sushi_amount))
+            if sushi_amount == 10:
+                v = 111 / 0
+        finally:
+            second_chopstick.release()
+            first_chopstick.release()
 
 
 if __name__ == '__main__':
     threading.Thread(target=ef, name='barron', args=(chopstick_a, chopstick_b)).start()
     threading.Thread(target=ef, name='olivia', args=(chopstick_b, chopstick_c)).start()
-    threading.Thread(target=ef, name='steeve', args=(chopstick_c, chopstick_a)).start()
+    threading.Thread(target=ef, name='steeve', args=(chopstick_a, chopstick_c)).start()
